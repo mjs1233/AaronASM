5431fdb2de6305dfac811bea3eda443359633acd
MJS1233 <apcbchet@gmail.com>
2023-09-17 21:46:44 +0900

    23/9/17_3

--- src/Includes/Register.h
+++ src/Includes/Register.h
@@ -12,6 +12,7 @@ struct Register
 	unsigned char NG : 1;
 	unsigned char OV : 1;
 	unsigned char CR : 1;
+	unsigned char INT : 1;//interrupt
 };
 
 void SetRegister(Register& reg, unsigned char register_select, unsigned int value);
